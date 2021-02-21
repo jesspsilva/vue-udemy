f475f9f1d5919142a2465e987d0bb2f9af29b7f1
Jéssica <jps936@hotmail.com>
2021-02-21 17:41:51 +0000

    Add assignment 6 files

---
+++ assignment-6/src/components/ActiveUser.vue
@@ -0,0 +1,10 @@
+<template>
+    <h2> {{ username }}</h2>
+    <h3> {{ age }} </h3>
+</template>
+
+<script>
+export default {
+    props: ['username', 'age']
+}
+</script>
---
+++ assignment-6/src/components/UserData.vue
@@ -0,0 +1,30 @@
+<template>
+    <form @submit.prevent="sendData">
+        <div>
+            <label>Name</label>
+            <input type="text" v-model="name"/>
+        </div>
+        <div>
+            <label>Age</label>
+            <input type="number" v-model="age"/>
+        </div>
+        <button>Update User</button>
+    </form>
+</template>
+
+<script>
+export default {
+    emits: ['send-data'],
+    data() {
+        return {
+            name: '',
+            age: null
+        }
+    },
+    methods: {
+        sendData() {
+            this.$emit('send-data', this.name, this.age);
+        }
+    }
+}
+</script>
